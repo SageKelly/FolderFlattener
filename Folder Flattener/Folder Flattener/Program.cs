ec0f3f8299c6e16429803cea21b06911d3ad47e9
SageKelly <musicaltech120@bellsouth.net>
2018-06-17 23:09:08 -0400

    Robust Update
    • Now checks to see if you're using the proper drives in the special file.

--- Folder Flattener/Folder Flattener/Program.cs
+++ Folder Flattener/Folder Flattener/Program.cs
@@ -48,50 +48,58 @@ namespace Folder_Flattener
             sr.Close();
             if (!Directory.Exists(baseDestination))
             {
-                Directory.CreateDirectory(baseDestination);
-            }
-
-            foreach (string file in musicList)
-            {
-                string fullDestination = "";
-                try
+                DriveInfo di = new DriveInfo(Directory.GetDirectoryRoot(baseDestination));
+                if (di.IsReady)
                 {
-                    if (!File.Exists(file))
+                    Directory.CreateDirectory(baseDestination);
+
+                    foreach (string file in musicList)
                     {
-                        SaveMissingFiles(file);
-                        continue;
+                        string fullDestination = "";
+                        try
+                        {
+                            if (!File.Exists(file))
+                            {
+                                SaveMissingFiles(file);
+                                continue;
+                            }
+                            int lastFileSlash = file.LastIndexOf('\\');
+                            int lastInputSlash = baseDestination.LastIndexOf('\\');
+
+
+                            fullDestination = baseDestination + "\\" + file.Substring(lastFileSlash + 1);
+
+                            if (lastFileSlash < file.Length && lastInputSlash < baseDestination.Length)
+                            {
+                                Console.Write("Writing {0} to {1}...", file.Substring(lastFileSlash + 1), baseDestination.Substring(lastInputSlash + 1));
+                            }
+                            if (!File.Exists(fullDestination))
+                            {
+                                File.Copy(file, fullDestination);
+                                Console.WriteLine("Done");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Cancelled: already exists.");
+                            }
+
+
+                        }
+                        catch (Exception E)
+                        {
+                            WriteException(GetException(E), fullDestination);
+                            break;
+                        }
                     }
-                    int lastFileSlash = file.LastIndexOf('\\');
-                    int lastInputSlash = baseDestination.LastIndexOf('\\');
-
-
-                    fullDestination = baseDestination + "\\" + file.Substring(lastFileSlash + 1);
-
-                    if (lastFileSlash < file.Length && lastInputSlash < baseDestination.Length)
-                    {
-                        Console.Write("Writing {0} to {1}...", file.Substring(lastFileSlash + 1), baseDestination.Substring(lastInputSlash + 1));
-                    }
-                    if (!File.Exists(fullDestination))
-                    {
-                        File.Copy(file, fullDestination);
-                        Console.WriteLine("Done");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Cancelled: already exists.");
-                    }
-
-
+                    Console.WriteLine("Writing Complete");
+                    WriteMissingFiles();
+                    Console.ReadKey(true);
                 }
-                catch (Exception E)
+                else
                 {
-                    WriteException(GetException(E), fullDestination);
-                    break;
+                    WriteException("I don't think you're using the right drive:", baseDestination);
                 }
             }
-            Console.WriteLine("Writing Complete");
-            WriteMissingFiles();
-            Console.ReadKey(true);
         }
 
         private static string GetException(Exception E)
